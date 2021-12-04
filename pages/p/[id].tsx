b8aebb21729c1ea178cf09232aae8026d961fdcd
Elisha Bello <elishabello2014@gmail.com>
2021-12-04 23:40:57 +0100

    Step 7. Add Publish functionality

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,9 +1,11 @@
-import React from "react"
-import { GetServerSideProps } from "next"
-import ReactMarkdown from "react-markdown"
-import Layout from "../../components/Layout"
-import { PostProps } from "../../components/Post"
-import prisma from "../../lib/prisma"
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import ReactMarkdown from 'react-markdown';
+import Layout from '../../components/Layout';
+import Router from 'next/router';
+import { PostProps } from '../../components/Post';
+import { useSession } from 'next-auth/client';
+import prisma from '../../lib/prisma';
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
@@ -12,7 +14,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     },
     include: {
       author: {
-        select: { name: true },
+        select: { name: true, email: true },
       },
     },
   });
@@ -20,23 +22,38 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     props: post,
   }
 }
-
+async function publishPost(id: number): Promise<void> {
+  await fetch(`http://localhost:3000/api/publish/${id}`, {
+    method: 'PUT'
+  });
+  await Router.push('/')
+}
 const Post: React.FC<PostProps> = (props) => {
+  const [session, loading] = useSession();
+  if (loading) {
+    return <div>Authenticating ...</div>;
+  }
+  const userHasValidSession = Boolean(session);
+  const postBelongsToUser = session?.user?.email === props.author?.email;
+
   let title = props.title
   if (!props.published) {
     title = `${title} (Draft)`
   }
 
   return (
-    <Layout>
-      <div>
-        <h2>{title}</h2>
-        <p>By {props?.author?.name || "Unknown author"}</p>
-        <ReactMarkdown source={props.content} />
-      </div>
-      <style jsx>{`
+      <Layout>
+        <div>
+          <h2>{title}</h2>
+          <p>By {props?.author?.name || 'Unknown author'}</p>
+          <ReactMarkdown source={props.content} />
+          {!props.published && userHasValidSession && postBelongsToUser && (
+              <button onClick={() => publishPost(props.id)}>Publish</button>
+          )}
+        </div>
+        <style jsx>{`
         .page {
-          background: white;
+          background: var(--geist-background);
           padding: 2rem;
         }
 
@@ -55,7 +72,7 @@ const Post: React.FC<PostProps> = (props) => {
           margin-left: 1rem;
         }
       `}</style>
-    </Layout>
+      </Layout>
   )
 }
 
